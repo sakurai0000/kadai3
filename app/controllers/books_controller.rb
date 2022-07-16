7187dd80f4e1486fb67fa768d6a6870f11044584
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-16 13:30:23 +0000

    wip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,56 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+   def create
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+     @books =Book.all
+     render :index
+    end
+   end
+
   def index
+     @books = Book.all
+     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+     redirect_to book_path(@book.id)
+    else
+       @books =Book.find(params[:id])
+       render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+    flash[:notice] = "successfully"
+    redirect_to books_path
+    
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  
+  
   def show
+  @user = User.find(params[:id])
+  @books = @user.books 
   end
 
   def edit
+   @user =  User.find(params[:id])
   end
+  
+  def index
+    @users = User.all
+  end
+  
 end
