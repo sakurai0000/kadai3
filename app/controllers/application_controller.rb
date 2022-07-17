360f962a98f8bda2975077c91b27ceb28fb2c212
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-17 14:59:01 +0000

    Initial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  #protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+   user_path(current_user)
+  end
+ 
   private
+  
+    
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
     end
+  
+
+  
 end
