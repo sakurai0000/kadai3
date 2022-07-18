e404560d2d14c0b75eb44a62b2d5aafa50bb973c
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-18 16:16:05 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,26 +3,31 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-   def create
+  def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-     flash[:notice] = "successfully"
+      flash[:notice] = "successfully"
       redirect_to book_path(@book.id)
     else
-     @books =Book.all
-     render :index
+      @books =Book.all
+      @user = current_user
+      render :index
     end
-   end
+  end
 
   def index
      @books = Book.all
      @book = Book.new
+     @user = current_user
+
   end
 
   def show
     @book = Book.find(params[:id])
     @books = Book.all
-    
+    @user = @book.user
+
   end
 
   def edit
@@ -45,12 +50,12 @@ class BooksController < ApplicationController
     if @book.destroy
     flash[:notice] = "successfully"
     redirect_to books_path
-    
+
     end
   end
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   
   def show
   @user = User.find(params[:id])
-  @books = @user.books 
+  @books = @user.books
+
   end
 
   def edit
@@ -12,6 +13,22 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+  
+    
+  def update
+    @user =  User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)  
+  end
+  
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)  
   end
   
 end
