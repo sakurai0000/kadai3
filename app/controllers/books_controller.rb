a80872125f4281942f70f560abf9279a900f1a24
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-19 11:56:02 +0000

    Rテスト前まで完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+
+ 
+  
   def new
     @book = Book.new
   end
@@ -7,7 +10,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "successfully"
+      flash[:notice] = "successfully!"
       redirect_to book_path(@book.id)
     else
       @books =Book.all
@@ -32,12 +35,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    render "edit"
+    else
+    redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "successfully"
+      flash[:notice] = "successfully!!"
      redirect_to book_path(@book.id)
     else
        @books =Book.find(params[:id])
@@ -57,5 +65,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,45 @@
 class UsersController < ApplicationController
-  
-  
+
+
+
   def show
   @user = User.find(params[:id])
   @books = @user.books.all
-  
+
 
   end
 
   def edit
    @user =  User.find(params[:id])
+   if @user == current_user
+    render "edit"
+   else
+    redirect_to user_path(current_user.id)
+
+   end
   end
-  
+
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
     @books = Book.all
   end
-  
-    
+
+
   def update
     @user =  User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(current_user.id)  
+    if @user.update(user_params)
+    redirect_to user_path(current_user.id), notice:'You have updated user successfully.'
+    else
+     render :edit
+    end
   end
-  
-  
+
+
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)  
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
