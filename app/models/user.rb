4d71099220d55ad0fc279ddc620435f882a043b1
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-08-09 08:57:21 +0000

    ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,14 @@ class User < ApplicationRecord
 
 
   has_one_attached :profile_image
+  
+  
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 
 
   def get_profile_image(width,height)
